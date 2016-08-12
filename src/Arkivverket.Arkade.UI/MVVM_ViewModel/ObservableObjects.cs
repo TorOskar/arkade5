7e1ccd7949e4862c5b5a45cc9fece2f36aae4a0e
Tor Oskar Ova Johnsen <toroskar@arkitektum.no>
2016-08-12 10:30:08 +0200

    First cut at a WPF MVVM based GUI scaffold

---
+++ src/Arkivverket.Arkade.UI/MVVM_ViewModel/ObservableObjects.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arkivverket.Arkade.UI.MVVM_ViewModel
+{
+    public class ObservableObjects : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ src/Arkivverket.Arkade.UI/MVVM_ViewModel/ViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Arkivverket.Arkade.UI.MVVM_ViewModel
+{
+    public class ViewModel : ObservableObjects
+    {
+
+        private string _textString;
+        private string _infoString;
+
+
+        public string TextString
+        {
+            get { return _textString; }
+            set
+            {
+                _textString = value;
+                OnPropertyChanged("TextString");
+            }
+        }
+
+        public string InfoString
+        {
+            get { return _infoString; }
+            set
+            {
+                _infoString = value;
+                OnPropertyChanged("InfoString");
+            }
+        }
+
+
+        public ICommand DoSomething
+        {
+            get { return new DelegateCommand(DoSomethingImplemented);}
+        }
+
+        private void DoSomethingImplemented()
+        {
+            InfoString = TextString.ToUpper();
+            TextString = string.Empty;
+        }
+    }
+}
